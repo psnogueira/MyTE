ab834be9ebe43fbd0065b32534b176c52c113ee3
psnogueira <psnogueira.e@gmail.com>
2024-05-15 15:38:32 -0300

    Index e Create dos Níveis de Acesso (Tabela AspNetRoles)

---
+++ MyTE/Controllers/RolesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTE.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _manager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager) 
+        {
+            _manager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _manager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IdentityRole role)
+        {
+            // Verificar se o Nível de Acesso já existe
+            if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+            {
+                _manager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyTE/Program.cs
+++ MyTE/Program.cs
@@ -10,9 +10,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
