16fdad8b4ffcc8e5212e83890f015489687da3f6
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-05-19 07:58:38 -0300

    Ajusta o DBInitializer para iniciar o programa com departamentos RH e TI, User Admin e roles Admin e User

--- MyTE/Models/ApplicationUser.cs
+++ MyTE/Models/ApplicationUser.cs
@@ -9,12 +9,12 @@ namespace MyTE.Models
         [Required(ErrorMessage ="O nome do funcionário é obrigatório.")]
         [Display(Name = "Nome")]
         [StringLength(50, ErrorMessage = "O nome do funcionário deve ter no máximo 50 caracteres.")]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Required(ErrorMessage = "O sobrenome do funcionário é obrigatório")]
         [MaxLength(50, ErrorMessage = "O sobrenome do funcionário deve ter até 50 caracteres")]
         [Display(Name = "Sobrenome")]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required(ErrorMessage = "A data de contratação é obrigatória.")]
         [DataType(DataType.Date)]
@@ -23,14 +23,14 @@ namespace MyTE.Models
 
         [Required(ErrorMessage = "O código PID do funcionário é obrigatório.")]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "O PID do funcionário tem apenas 11 caracteres")]
-        public string? PID { get; set; }
+        public string PID { get; set; }
 
         [Required(ErrorMessage = "O departamento é obrigatório.")]
         [ForeignKey("Department")]
         [Display(Name = "Departamento")]
         public int DepartmentId { get; set; }
 
-        public virtual Department? Department { get; set; }
+        public virtual Department Department { get; set; }
 
     }
 }
