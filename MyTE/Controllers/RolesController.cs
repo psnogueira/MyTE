1641b4a7a5e75c7812151b5e16a83bf20abe46a1
Alexia Nathalia Bitencourt Henrique <nathaliabitencourt03@gmail.com>
2024-05-19 12:55:11 -0300

    Adicionado AutoComplete na View Roles e Implementação das demais views

--- MyTE/Controllers/RolesController.cs
+++ MyTE/Controllers/RolesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace MyTE.Controllers
 {
@@ -16,17 +17,37 @@ namespace MyTE.Controllers
             _manager = roleManager;
         }
 
-
         public async Task<IActionResult> Index(string searchString)
         {
             ViewData["CurrentFilter"] = searchString;
-            var roles = from s in _manager.Roles select s;
-            if(!String.IsNullOrEmpty(searchString))
+            var roles = from r in _manager.Roles select r;
+            if (!String.IsNullOrEmpty(searchString))
             {
-                roles = roles.Where(s=> s.Id.Contains(searchString) || s.Name.Contains(searchString));
+                roles = roles.Where(r => r.Id.Contains(searchString) || r.Name.Contains(searchString));
             }
-    
+
             return View(await roles.ToListAsync());
+        }
+
+        [Produces("application/json")]
+        [HttpGet("search")]
+        public IActionResult AutoComplete()
+        {
+            try
+            {
+                string term = HttpContext.Request.Query["term"].ToString();
+                var roleName = _manager.Roles
+                    .Where(r => r.Name.Contains(term) || r.Id.Contains(term))
+                    .Select(r => r.Name)
+                    .ToList();
+
+                return Ok(roleName);
+
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
 
         [HttpGet]
@@ -36,7 +57,8 @@ namespace MyTE.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(IdentityRole role)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id", "Name")] IdentityRole role)
         {
             // Verificar se o Nível de Acesso já existe
             if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
@@ -84,7 +106,7 @@ namespace MyTE.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, IdentityRole role)
+        public async Task<IActionResult> Edit(string id, [Bind("Id", "Name")] IdentityRole role)
         {
             if (id != role.Id)
             {
