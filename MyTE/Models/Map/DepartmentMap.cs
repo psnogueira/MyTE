f98ea52ddaf65afc5c5b47cc06177032a2401bd9
psnogueira <psnogueira.e@gmail.com>
2024-05-24 10:50:57 -0300

    Gerador de arquivos .csv de Departamentos com Encoding UTF-8.

---
+++ MyTE/Models/Map/DepartmentMap.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CsvHelper.Configuration;
+
+namespace MyTE.Models.Map
+{
+    public class DepartmentMap : ClassMap<Department>
+    {
+        public DepartmentMap()
+        {
+            Map(m => m.DepartmentId).Name("Id");
+            Map(m => m.Name).Name("Nome");
+        }
+    }
+}
