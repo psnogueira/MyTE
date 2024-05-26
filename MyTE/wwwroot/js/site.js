4eeb50f2cfa31559fd49702c713e5f2fa92c20ee
Alexia Nathalia Bitencourt Henrique <nathaliabitencourt03@gmail.com>
2024-05-26 16:52:15 -0300

    1- Corrigido erro que mostrava "email já existe" ao tentar editar o usuário; 2- Adicionado .active para mostrar a página em que o usuário esta 3- PID gerado automaticamente

--- MyTE/wwwroot/js/site.js
+++ MyTE/wwwroot/js/site.js
@@ -2,30 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function () {
-    // Obtenha os valores dos atributos data-* no input de pesquisa
-    var inputElement = $("#autoComplete");
-    var tableName = inputElement.data("table");
-    var columnName1 = inputElement.data("column1");
-    var columnName2 = inputElement.data("column2");
-
-    inputElement.autocomplete({
-        source: function (request, response) {
-            var url = 'api/autocomplete/' + tableName + '/search';
-            var data = { term: request.term, columnName1: columnName1 };
-
-            if (columnName2) {
-                data.columnName2 = columnName2;
-            }
-
-            $.ajax({
-                url: url,
-                data: data,
-                success: function (data) {
-                    response(data);
-                }
-            });
-        }
-    });
-});
-
