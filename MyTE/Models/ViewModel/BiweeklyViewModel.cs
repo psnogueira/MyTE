ae1c09ebcf481a35310370a43d8f388e7434890a
Alexia Nathalia Bitencourt Henrique <nathaliabitencourt03@gmail.com>
2024-05-27 20:56:42 -0300

    AdminView funcionando com wbs

---
+++ MyTE/Models/ViewModel/BiweeklyViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MyTE.Models.ViewModel
+{
+    public class AdminRecordViewModel
+    {
+        public string UserEmail { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double TotalHours { get; set; }
+        public List<RecordWithWBS> RecordsWithWBS { get; set; }
+    }
+
+    public class RecordWithWBS
+    {
+        public DateTime Data { get; set; }
+        public double Hours { get; set; }
+        public string WBSName { get; set; }
+        public string WBSDescription { get; set; }
+    }
+}
