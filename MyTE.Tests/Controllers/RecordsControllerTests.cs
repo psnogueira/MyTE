48f155ec117311a2800c64bb0e3e86f7337e66ba
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-05-28 14:55:37 -0300

    Acrescenta testes para o método persist

--- MyTE.Tests/Controllers/RecordsControllerTests.cs
+++ MyTE.Tests/Controllers/RecordsControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
@@ -49,11 +50,18 @@ namespace MyTE.Tests.Controllers
             var userManager = GetUserManagerMock();
             var controller = new RecordsController(context, userManager);
 
+            var testWbsCode = "001";
+            var testHours = 8;
+
+            // Limpa o banco de dados antes de cada teste
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
             // Adiciona dados de exemplo ao banco de dados
             context.WBS.Add(new WBS
             {
                 WBSId = 1,
-                Code = "001",
+                Code = testWbsCode,
                 Desc = "Description1",
                 Type = (WBSType) 1
             });
@@ -61,7 +69,7 @@ namespace MyTE.Tests.Controllers
             {
                 RecordId = 1,
                 Data = DateTime.Now.Date,
-                Hours = 8,
+                Hours = testHours,
                 UserId = "test-user-id",
                 WBSId = 1
             });
@@ -78,10 +86,143 @@ namespace MyTE.Tests.Controllers
             Assert.NotNull(model);
             Assert.Single(model);
             //Verifica se o viewmodel contém os dados esperados
-            Assert.Equal("001", model.First().WBS.Code);
-            Assert.Equal(8, model.First().TotalHours);
+            Assert.Equal(testWbsCode, model.First().WBS.Code);
+            Assert.Equal(testHours, model.First().TotalHours);
 
-            // Obs.: Esse teste verifica se o método Index retorna os registros corretos para da data atual quando é chamado sem uma data específica
+            // Obs.: Esse teste verifica se o método Index retorna os registros corretos para a data atual quando é chamado sem uma data específica
         }
+
+        //[Fact]
+        //public async Task Index_ReturnsCorrectViewWithSpecificDate_WhenDataSearchIsInvalid()
+        //{
+        //   TODO: desenvolver teste para lidar com entrada de data menor que o StartDateRestriction    
+        //}
+
+        [Fact]
+        public async Task Index_ReturnsCorrectViewWithDefaultDate_WhenDataSearchIsValid()
+        {
+            // ARRANGE
+            var context = GetDbContext();
+            var userManager = GetUserManagerMock();
+            var controller = new RecordsController(context, userManager);
+
+            var testWbsCode = "001";
+            var testHours = 8;
+            var testDate = DateTime.Now.Date;
+
+            // Limpa o banco de dados antes de cada teste
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Adiciona dados de exemplo ao banco de dados
+            context.WBS.Add(new WBS
+            {
+                WBSId = 1,
+                Code = testWbsCode,
+                Desc = "Description1",
+                Type = (WBSType)1
+            });
+            context.Record.Add(new Models.Record
+            {
+                RecordId = 1,
+                Data = testDate,
+                Hours = testHours,
+                UserId = "test-user-id",
+                WBSId = 1
+            });
+            context.SaveChanges();
+
+            // ACT
+            var result = await controller.Index(testDate) as ViewResult;
+
+            // ASSERT
+
+            // Verifica se o resultado não é nulo
+            Assert.NotNull(result);
+            var model = result.Model as List<RecordDTO>;
+            Assert.NotNull(model);
+            Assert.Single(model);
+            //Verifica se o viewmodel contém os dados esperados
+            Assert.Equal(testWbsCode, model.First().WBS.Code);
+            Assert.Equal(testHours, model.First().TotalHours);
+
+            // Obs.: Esse teste verifica se o método Index retorna os registros corretos quando é chamado com a data atual
+        }
+
+        [Fact]
+        public async Task Persist_RedirecsToIndex_WithValidRecords()
+        {
+            // ARRANGE
+            var context = GetDbContext();
+            var userManager = GetUserManagerMock();
+            var controller = new RecordsController(context, userManager);
+
+            // Limpa o banco de dados antes de cada teste
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var validRecords = new List<RecordDTO>
+            {
+                new RecordDTO
+                {
+                    records = new List<Models.Record>
+                    {
+                        new Models.Record { UserId = "test-user-id", Data = new DateTime(2024, 5, 2), Hours = 8, WBSId = 1},
+                        new Models.Record { UserId = "test-user-id", Data = new DateTime(2024, 5, 3), Hours = 8, WBSId = 1},
+                    },
+                    WBSId = 1,
+                    WBS = new WBS { WBSId = 1, Code = "001", Desc = "Description1", Type = (WBSType)1 },
+                    TotalHours = 16,
+                    TotalHoursDay = new double[16]
+                }
+            };
+
+            // ACT
+            var result = await controller.Persist(validRecords);
+
+            // ASSERT
+            Assert.IsType<RedirectToActionResult>(result);
+            var redirectToAction = (RedirectToActionResult)result;
+            Assert.Equal("Index", redirectToAction.ActionName);
+        }
+
+
+        //  AJUSTAR PARA RECEBER O ERRO TEMPDATA QUE EXCEDE O NUMERO DE HORAS
+        //[Fact]
+        //public async Task Persist_RedirectsToIndex_WithInvalidRecords()
+        //{
+        //    // ARRANGE
+        //    var context = GetDbContext();
+        //    var userManager = GetUserManagerMock();
+        //    var controller = new RecordsController(context, userManager);
+
+        //    // Limpa o banco de dados antes de cada teste
+        //    context.Database.EnsureDeleted();
+        //    context.Database.EnsureCreated();
+
+        //    var invalidRecords = new List<RecordDTO>
+        //    {
+        //        new RecordDTO
+        //        {
+        //            records = new List<Models.Record>
+        //            {
+        //                new Models.Record { UserId = "test-user-id", Data = new DateTime(2024, 5, 2), Hours = 4, WBSId = 1},
+        //                new Models.Record { UserId = "test-user-id", Data = new DateTime(2024, 5, 3), Hours = 25, WBSId = 1},
+        //            },
+        //            WBSId = 1,
+        //            WBS = new WBS { WBSId = 1, Code = "001", Desc = "Description1", Type = (WBSType)1 },
+        //            TotalHours = 33,
+        //            TotalHoursDay = new double[16]
+        //        }
+        //    };
+
+        //    // ACT
+        //    var result = await controller.Persist(invalidRecords);
+
+        //    // ASSERT
+        //    Assert.IsType<RedirectToActionResult>(result);
+        //    var redirectToAction = (RedirectToActionResult)result;
+        //    Assert.Equal("Index", redirectToAction.ActionName);
+        //}
     }
 }
